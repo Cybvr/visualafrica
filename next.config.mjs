9ea595984294cab0754da70f17f877767258a663
cybvr <jide@visual.ng>
2026-02-28 04:55:32 +0100

    fix: pwa install popup not showing by fixing icon sizes and manifest configuration

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const withPWA = withPWAInit({
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
   swMinify: true,
-  disable: process.env.NODE_ENV === 'development',
+  disable: false,
   workboxOptions: {
     disableDevLogs: true,
   },
